ce69649ede574b6a9c8cad5f5e410b5f076d836d
Hilmi Atha Putra <108039453+hilmiatha@users.noreply.github.com>
2023-11-20 12:15:54 +0700

    connecting to flutter

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json,show_xml_by_id,show_json_by_id,register, login_user, logout_user, get_product_json, add_product_ajax,delete_product_ajax
-
+from main.views import show_main, create_product, show_xml, show_json,show_xml_by_id,show_json_by_id,register, login_user, logout_user, get_product_json, add_product_ajax,delete_product_ajax, create_product_flutter
 app_name = 'main'
 
 urlpatterns = [
@@ -16,4 +15,5 @@ urlpatterns = [
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-ajax/', add_product_ajax, name='add_product_ajax'),
     path('delete-ajax/<int:product_id>/', delete_product_ajax, name='delete_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from main.forms import ProductForm
@@ -145,3 +146,25 @@ def delete_product_ajax(request,product_id):
         return HttpResponse(b"OK", status=200)
 
     return HttpResponseNotFound()
+
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_product = Item.objects.create(
+            user = request.user,
+            name = data["name"],
+            price = int(data["price"]),
+            description = data["description"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+
+
